f73f2bb877c9c947f99abb3156b52ea403b0765c
Alisher <todjiev@gmail.com>
2019-06-04 22:16:27 +0700

    Update css

--- src/app/components/people/people.module.ts
+++ src/app/components/people/people.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from '@angular/material/card';
 import { PeopleListComponent } from "./people-list/people-list.component";
 import { PeopleListItemComponent } from "./people-list-item/people-list-item.component";
 import { PeopleContainerComponent } from "./people-container/people-container.component";
@@ -14,6 +15,6 @@ import { PeopleContainerComponent } from "./people-container/people-container.co
     PeopleContainerComponent
   ],
   exports: [PeopleListComponent,PeopleContainerComponent],
-  imports: [CommonModule, MatListModule, MatIconModule, MatButtonModule]
+  imports: [CommonModule, MatListModule, MatIconModule, MatButtonModule, MatCardModule]
 })
 export class PeopleModule {}
