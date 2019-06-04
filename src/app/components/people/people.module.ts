7e038ec8ddc3eaeb37a2b8a49dffff55c4a57de3
Alisher <todjiev@gmail.com>
2019-06-04 22:23:05 +0700

    Add component

--- src/app/components/people/people.module.ts
+++ src/app/components/people/people.module.ts
@@ -7,12 +7,14 @@ import { MatCardModule } from '@angular/material/card';
 import { PeopleListComponent } from "./people-list/people-list.component";
 import { PeopleListItemComponent } from "./people-list-item/people-list-item.component";
 import { PeopleContainerComponent } from "./people-container/people-container.component";
+import { PeopleDetailsComponent } from './people-details/people-details.component';
 
 @NgModule({
   declarations: [
     PeopleListComponent,
     PeopleListItemComponent,
-    PeopleContainerComponent
+    PeopleContainerComponent,
+    PeopleDetailsComponent
   ],
   exports: [PeopleListComponent,PeopleContainerComponent],
   imports: [CommonModule, MatListModule, MatIconModule, MatButtonModule, MatCardModule]
